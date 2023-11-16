c6706f42be806ebde8b6560ebef460909895e502
prabhjyot9 <prabhjyot.wadhwa@gmail.com>
2023-11-16 22:22:09 +0530

    Component Keyboard Added

---
+++ src/components/ComponentKeyboard/ComponentKeyboard.stories.js
@@ -0,0 +1,12 @@
+import { ComponentKeyboard } from ".";
+
+export default {
+  title: "Components/ComponentKeyboard",
+  component: ComponentKeyboard,
+};
+
+export const Default = {
+  args: {
+    className: {},
+  },
+};
---
+++ src/components/ComponentKeyboard/index.ts
@@ -0,0 +1,1 @@
+export { ComponentKeyboard } from "./ComponentKeyboard";
