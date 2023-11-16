f5de7a7bcbebe809631c5d675e4194deef1453ca
prabhjyot9 <prabhjyot.wadhwa@gmail.com>
2023-11-16 22:25:10 +0530

    Icon Button Added

---
+++ src/components/IconButton/IconButton.stories.js
@@ -0,0 +1,24 @@
+import { IconButton } from ".";
+
+export default {
+  title: "Components/IconButton",
+  component: IconButton,
+  argTypes: {
+    configuration: {
+      options: ["filled", "outlined", "tonal", "standard"],
+      control: { type: "select" },
+    },
+    state: {
+      options: ["enabled", "pressed", "focused", "hovered", "disabled"],
+      control: { type: "select" },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    configuration: "filled",
+    state: "enabled",
+    className: {},
+  },
+};
